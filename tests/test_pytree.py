3db959b57e626409c3ed9e5167223412b2f59c01
Yan Qian <qianyan@thoughtworks.com>
2018-06-24 09:35:18 +0800

    tree format - add one more file in sub directory

--- tests/test_pytree.py
+++ tests/test_pytree.py
@@ -4,4 +4,6 @@ test_pytree.py
 import pytree.core as pytree
 
 def test_tree_format():
-    assert pytree.tree_format('tests', 'fixtures') == {'name': 'fixtures', 'children': []}
+    actual_tree = pytree.tree_format('tests', 'fixtures') 
+    expected_tree = {'name': 'fixtures', 'children': [{'name': 'child', 'children': []}]}
+    assert actual_tree == expected_tree
